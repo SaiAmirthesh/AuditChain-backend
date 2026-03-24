82ecf6cb252bfc57b9af5a9602274fae43b81994
SaiAmirthesh <saiamirthesh8419@gmail.com>
2026-03-24 20:17:31 +0530

    init: first commit

---
+++ src/main/java/com/SaiAmirthesh/AuditChain/AuditChainApplication.java
@@ -0,0 +1,13 @@
+package com.SaiAmirthesh.AuditChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditChainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SaiAmirthesh/AuditChain/AuditChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SaiAmirthesh.AuditChain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
